07afdb9858faee51c9eeada39e49011462f348fc
Guilherme Brisson <54915600+guiBrisson@users.noreply.github.com>
2024-01-24 19:11:48 -0300

    Add roadmap icon

--- app/src/main/java/com/github/guibrisson/roadmaps/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/github/guibrisson/roadmaps/ui/screen/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,7 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
@@ -70,11 +73,21 @@ internal fun HomeScreen(
         }
     ) {
         item {
-            Text(
+            Row(
                 modifier = Modifier.padding(top = 40.dp, bottom = 24.dp),
-                text = stringResource(id = R.string.app_name),
-                style = MaterialTheme.typography.titleMedium,
-            )
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_roadmap),
+                    contentDescription = stringResource(id = R.string.roadmap_icon_content_description)
+                )
+
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
         }
 
         when (uiState) {
